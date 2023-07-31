b42eb6a414e23a493914354720343fa460800d9c
samckp <sam.mca.liet@gmail.com>
2023-07-31 08:05:17 +0530

    file uploader added

--- streamlit_widget.py
+++ streamlit_widget.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+from PIL import Image
 
 def changed():
     print("Changed !")
@@ -15,7 +15,17 @@ def click():
     print("Submitted")
 btn = st.button("Submit" , on_click=click)
 
+# select box / combo box
 select = st.selectbox("Cars", options=("Audi", "Merc","Tata", "Kia", "Toyota"))
 print(select)
 
+#Multiselect box
 multi_select = st.multiselect("Favourite Language", options=("French", "Dutch", "Eng", "Hindi","Spanish"))
+
+## file uploader
+ 
+images = st.file_uploader("Upload images", type=['png', 'jpg'])
+image = Image.open(images)
+st.image(image)
+
+           
