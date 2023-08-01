8f462208c8cae1b4f497fc4c76e41b811296e2dd
samckp <sam.mca.liet@gmail.com>
2023-08-01 18:15:00 +0530

    password masked

--- streamlit_form.py
+++ streamlit_form.py
@@ -1,13 +1,13 @@
 import streamlit as st
 
-st.markdown("<h1>User Registration </h1>")
+st.title("User Registration")
 
 with st.form("form1"):
     col1,col2 = st.columns(2)
     col1.text_input("First_Name")
     col2.text_input("Last_Name")
     st.text_input("Email")
-    st.text_input("Password")
-    st.text_input("Confirm Password")
+    st.text_input("Password", type='password')
+    st.text_input("Confirm Password",type='password')
     st.form_submit_button("Submit")
 
