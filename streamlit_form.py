755b0d0cd80bb7c6e4fac8055e7aedc95118bb52
samckp <sam.mca.liet@gmail.com>
2023-08-03 18:49:18 +0530

    added editable table

--- streamlit_form.py
+++ streamlit_form.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.title("User Registration")
 
@@ -14,3 +15,21 @@ with st.form("form1"):
 
 st.divider()
 st.text("*******************************developed by Shyamu Sharma*******************************")    
+
+st.divider()
+
+df = pd.DataFrame(
+    [
+       {"Company": "Google", "rating": 4, "is_mnc": True},
+       {"Company": "Amazon", "rating": 5, "is_mnc": False},
+       {"Company": "Meta", "rating": 3, "is_mnc": True},
+       {"Company": "Microsoft", "rating": 3, "is_mnc": True},
+       {"Company": "Pwc", "rating": 4, "is_mnc": True},
+       {"Company": "ACC", "rating": 2, "is_mnc": False},
+       {"Company": "Kyros", "rating": 1, "is_mnc": False},
+   ]
+)
+edited_df = st.data_editor(df, num_rows="dynamic")
+
+favorite_command = edited_df.loc[edited_df["rating"].idxmax()]["Company"]
+st.markdown(f"Your favorite Company is **{favorite_command}** 🎈")
