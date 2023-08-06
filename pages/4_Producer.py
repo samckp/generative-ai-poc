96e2aceec6438ca60ca5d439787a716797e05608
samckp <sam.mca.liet@gmail.com>
2023-08-06 21:28:21 +0530

    Producer menu created

--- pages/4_Producer.py
+++ pages/4_Producer.py
@@ -5,14 +5,34 @@ from time import sleep
 from json import dumps
 from kafka import KafkaProducer
 
-st.title('Producer')
 
-topic_name='mytopic'
-producer = KafkaProducer(bootstrap_servers=['localhost:9092'], value_serializer=lambda x:
-dumps(x).encode('utf-8'))
+def producer():
+    producer = KafkaProducer(bootstrap_servers=['localhost:9092'], value_serializer=lambda x:
+    dumps(x).encode('utf-8'))
 
-for e in range(10):
-    data = {'number' : e}
-    print(data)
-    producer.send(topic_name, value=data)
-    sleep(0.005)
+    for e in range(nofm):
+        data = {msg_format : e}
+        st.write(data)
+        producer.send(topic, value=data)
+        sleep(0.005)
+
+
+st.title('********Producer********')
+
+with st.form("form1", clear_on_submit=True):
+    col1,col2 = st.columns(2)
+    topic = col1.text_input("Topic Name")
+    msg = col2.text_input("Number of Messages")
+    msg_format = col1.text_input("Message Format")
+
+    nofm = int(msg)   
+    sub_state = st.form_submit_button("Produce")
+    st.divider()
+    
+    if sub_state:
+        if topic == "" and nofm >0:
+            st.warning("Please fill above fields")
+        else:
+            producer()
+            st.success("Successfully Published !!")
+    
