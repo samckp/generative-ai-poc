cd538bdd5c2c9b574f194ce55a40f5e22e69e71e
samckp <sam.mca.liet@gmail.com>
2023-08-08 08:30:09 +0530

    Int issue resolved

--- pages/4_Producer.py
+++ pages/4_Producer.py
@@ -10,7 +10,7 @@ def producer():
     producer = KafkaProducer(bootstrap_servers=['localhost:9092'], value_serializer=lambda x:
     dumps(x).encode('utf-8'))
 
-    for e in range(nofm):
+    for e in range(msg):
         data = {msg_format : e}
         st.write(data)
         producer.send(topic, value=data)
@@ -22,15 +22,15 @@ st.title('********Producer********')
 with st.form("form1", clear_on_submit=True):
     col1,col2 = st.columns(2)
     topic = col1.text_input("Topic Name")
-    msg = col2.text_input("Number of Messages")
+    msg = col2.number_input("Number of Messages", min_value=1, max_value=9999)
     msg_format = col1.text_input("Message Format")
-
-    nofm = int(msg)   
+      
+      
     sub_state = st.form_submit_button("Produce")
     st.divider()
-    
+
     if sub_state:
-        if topic == "" and nofm >0:
+        if topic == "" and msg >0:
             st.warning("Please fill above fields")
         else:
             producer()
